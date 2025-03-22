84961687b74db96efa1835880525e2211fd91372
unknown <minhajsulmuneeb@gmail.com>
2025-03-22 16:55:18 +0500

    0.0.2

---
+++ index.js
@@ -0,0 +1,13 @@
+// exports.printMsg = function() {
+//     console.log("This is a message from the test package syed-minhaj/test-lib ");
+// }
+
+function console_test(){
+    console.log("This is a message from the test package syed-minhaj/test-lib ");
+}
+
+function abc_test(a){
+    console.log({`this ${a} is from module`})
+}
+
+module.exports = {console_test , abc_test};
